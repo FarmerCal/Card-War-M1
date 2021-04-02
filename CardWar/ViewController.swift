3b5e74db34ca4ea1f746ab98b35057d86e7771af
callum day ham <caldayham@gmail.com>
2021-04-02 01:40:33 -0700

    Initial Commit

---
+++ CardWar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardWar
+//
+//  Created by Callum Day Ham on 4/2/21.
+//  Copyright © 2021 com.farmercal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
